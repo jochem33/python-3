3559097ff82068f204c96fb42b7f96f54ea81b7b
jochem33 <jochemjuriaan.baas@coderclass.nl>
2018-08-31 16:48:41 +0200

    overhoor functie af

--- main.py
+++ main.py
@@ -79,16 +79,60 @@ def overhoor():
     for item in recentelijsten:
         print(str(i + 1) + ". " + recentelijsten[i])
         i += 1
-    print("\n")
 
     print("Kies een getal van een recente file of geef een filenaam op")
+
     keuze = input()
+    gekozenfile = ""
+
     if keuze.isdigit():
-        int(keuze) -= 1
+        keuze = int(keuze)
+        keuze -= 1
+
+        with open(recentelijstenfilenaam) as recentelijstenfile:
+            bestandsdata = recentelijstenfile.read().split("\n")
+
+        if os.path.isfile(bestandsdata[keuze]):
+            gekozenfile = bestandsdata[keuze]
+        else:
+            print("[ERROR] 404 file not found")
+
     elif os.path.isfile(keuze):
-        print("Hier komt nog iets")
+        gekozenfile = keuze
+
     else:
         print("We hebben je keuze niet kunnen vinden")
+
+    if gekozenfile != "":
+        print("We gaan nu " + gekozenfile + " overhoren.")
+
+        overhoordict = {}
+        goed = 0
+        fout = 0
+
+        with open(gekozenfile) as overhoorfile:
+            bestandsdata = overhoorfile.read().split('\n')
+
+        for item in bestandsdata:
+            if not item == '':
+                woord1, woord2 = item.split("=")
+                overhoordict[woord1] = woord2
+
+        for key in overhoordict:
+            print("\n" * 2)
+            geradenwoord = input(key + " = ")
+            print("\n" * 2)
+
+            if geradenwoord == overhoordict[key]:
+                print("Dat is goed!")
+                goed += 1
+            else:
+                print("Helaas! Dat is fout, het moest zijn: " + overhoordict[key])
+                fout += 1
+
+        totaalaantalvragen = goed + fout
+        score = goed / totaalaantalvragen * 100
+        print("Je hebt " + str(score) + "% goed!")
 
     main()
 
